9b7c2b482ea1bb024fcff765e251b3ced902f445
Ritesh Bhat <riteshbhat17@gmail.com>
2019-02-03 00:44:51 +0530

    Delete time_wrapper.py

--- pydekho/time_wrapper.py
+++
@@ -1,9 +0,0 @@
-import time
-
-def time_wrapper(func):
-    def wrapper(*args, **kwargs):
-        start = time.time()
-        res = func(*args, **kwargs)
-        print("Time taken to execute :: ",time.time()-start)
-        return res
-    return wrapper
